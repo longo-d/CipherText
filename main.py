2072065ea9bc459605f315bc60ada5c336875ab8
Daniel Longo <long0266@algonquinlive.com>
2023-02-17 17:21:10 -0500

    making functions

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+# function for encryption
+def encrypt():
+    # get the index of each character 
+    # in the message from the regular alphabet
+    indexMsg = []
+    for i in msg:
+        indexMsg.append(alphabet.index(i))
+
+    # translate the index of each regular character
+    # to the ciphered alphabet character
+    cipherArray = []
+    for i in indexMsg:
+        cipherArray.append(cipherAlphabet[i])
+
+    # join the cipherArray together to form the ciphertext as a string
+    cipherText = ''.join(cipherArray)
+
+    return(cipherText)
+
+# function for decryption
+def decrypt():
+    # get the index of each character
+    # in the message from the cipher alphabet 
+    decryptMsg = []
+    for i in msg:
+        decryptMsg.append(cipherAlphabet.index(i))
+
+    # translate the index of each ciphered character
+    # to the regular alphabet character
+    plainArray = []
+    for i in decryptMsg:
+        plainArray.append(alphabet[i])
+
+    # join the plainArray together to form the plaintext as a string
+    plainText = ''.join(plainArray) 
+
+    return(plainText)
+
 # explain the application
 print("Welcome to CipherText!")
 print("The best method of converting plaintext to ciphertext and ciphertext to plaintext!")
@@ -6,6 +44,9 @@ print("The best method of converting plaintext to ciphertext and ciphertext to p
 message = input("Enter a message: ")
 key = int(input("Enter a key: "))
 operation = input("Enter encrypt or decrypt: ")
+
+# output
+outputMessage = ""
 
 # alphabet sequence
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
@@ -40,45 +81,13 @@ for char in message:
 
 # print(msg)
 if operation == "encrypt":
-    # get the index of each character 
-    # in the message from the regular alphabet
-    indexMsg = []
-    for i in msg:
-        indexMsg.append(alphabet.index(i))
-
-    # print(indexMsg)
-
-    # translate the index of each regular character
-    # to the ciphered alphabet character
-    cipherArray = []
-    for i in indexMsg:
-        cipherArray.append(cipherAlphabet[i])
-
-    # print(cipherArray)
-
-    # join the cipherArray together to form the ciphertext as a string
-    cipherText = ''.join(cipherArray)
-    print(cipherText)
+    outputMessage = encrypt()
 
 elif operation == "decrypt":
-    # get the index of each character
-    # in the message from the cipher alphabet 
-    decryptMsg = []
-    for i in msg:
-        decryptMsg.append(cipherAlphabet.index(i))
-
-    # print(decryptMsg)
-
-    # translate the index of each ciphered character
-    # to the regular alphabet character
-    plainArray = []
-    for i in decryptMsg:
-        plainArray.append(alphabet[i])
-
-    # join the plainArray together to form the plaintext as a string
-    plainText = ''.join(plainArray) 
-    print(plainText)
+    outputMessage = decrypt()
 
 # catch invalid operation
 else:
     print("invalid operation method")
+
+print(outputMessage)
